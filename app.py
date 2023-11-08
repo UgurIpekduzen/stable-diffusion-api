5e932d71091cdde37794d86df388012d154ba1f2
UgurIpekduzen <u.ipkdzn@gmail.com>
2023-11-08 23:29:51 +0300

    bootstrap ui added

--- app.py
+++ app.py
@@ -1,101 +1,65 @@
-# from flask import Flask, request, jsonify, send_file, Response
-# from PIL import Image
-# from io import BytesIO
-# import base64
-# import torch
+from flask import Flask, request, jsonify, send_file, render_template
+import torch
+import os
+from src.tasks import generate_ad, generate_img
+import json
 
-# from PIL import Image
-# from io import BytesIO
-# from tasks import generate_ad, generate_img
+assert torch.cuda.is_available()
+torch.cuda.empty_cache()
 
-# assert torch.cuda.is_available()
+with open("config.json") as f:
+    cfg = json.loads(f.read())
 
-# torch.cuda.empty_cache()
+app = Flask(__name__, template_folder='./templates/')
+app.config['UPLOAD_FOLDER'] = cfg['image_upload_folder']
+# @app.route('/generate-image', methods=['POST'])
 
-# app = Flask(__name__)
+# def generate_image():
+#     try:
+#         prompt = request.form['prompt']
+#         image = request.files['image']
+#         aiimg_hexcode = request.form['aiimg_hexcode']
+        
+#         gen_image = generate_img(image, prompt, aiimg_hexcode)
 
+#         save_path = os.path.join(cfg['image_path'], cfg['gen_img_name'])
+#         gen_image.save(save_path)
+        
+#         return send_file(save_path, mimetype="image/png")
+#     except Exception as e:
+#         return jsonify({"error": str(e)})  
+    
 # @app.route('/img2img-api', methods=['POST'])
-
 # def img2img_api():
 #     try:
 #         # Get the inputs from the request
 #         prompt = request.form['prompt']
 #         image = request.files['image']
 #         aiimg_hexcode = request.form['aiimg_hexcode']
-        
-#         # logo = request.files['logo']
-#         # adimg_hexcode = request.form['adimg_hexcode']
-#         # punchline = request.form['punchline']
-#         # button = request.form['button']
+#         logo = request.files['logo']
+#         adimg_hexcode = request.form['adimg_hexcode']
+#         punchline = request.form['punchline']
+#         button = request.form['button']
 
-#         gen_image = generate_img(image=image, prompt=prompt, hexcode=aiimg_hexcode)
-#         if gen_image:
-#             output_buffer = BytesIO()
-#             gen_image.save(output_buffer, format="PNG")
-#             response = Response(output_buffer.getvalue(), mimetype="image/png")
-#             return response
-#         else:
-#             return jsonify({"error": "Image generation failed."})
-#         # img_name = generate_ad(image=gen_image, logo=logo, hexcode=adimg_hexcode, punchline=punchline, button=button)
-        
-#         # return send_file('image.html', gen_image=img_name)
+#         gen_image = generate_img(image, prompt, aiimg_hexcode)
+#         ad_image = generate_ad(gen_image, logo, adimg_hexcode, punchline, button)
 
+#         save_path = os.path.join(cfg['image_path'], cfg['ad_img_name'])
+#         ad_image.save(save_path)
+        
+#         return send_file(save_path, mimetype="image/png")
 #     except Exception as e:
 #         return jsonify({"error": str(e)})
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8080)
-# # @app.route('/generate-ad', methods=['POST'])
 
-# # def generate_ad():
-# #     try:
-# #         # Get the inputs from the request
-# #         prompt = request.form['prompt']
-# #         logo = request.files['logo']
-# #         punchline = request.form['punchline']
-# #         hexcode = request.form['hexcode']
-# #         button = request.form['button']
+@app.route('/images/<filename>')
 
-# #         if logo and allowed_file(logo.filename):
-# #             # Convert and process the image
-# #             logo = Image.open(logo)
-# #             logo = logo.convert("RGB")
-# #             logo = logo.resize((768, 768))
+def show_result(filename):
+    return send_file(os.path.join(cfg['image_upload_folder'], filename), mimetype='image/png')
 
-# #             buffered = BytesIO()
-# #             output.save(buffered, format="PNG")
-# #             img_str = base64.b64encode(buffered.getvalue())
-# #             return jsonify({"ad_image": img_str.decode('utf-8')})
-            
-# #             # img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-# #             # return render_template('image.html', gen_image=img_str)
-
-# #         else:
-# #             return jsonify({"error": "Invalid image format or missing image"})
-
-# #     except Exception as e:
-# #         return jsonify({"error": str(e)})
-
-# # Parameters for the ad template
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8080)
-
-from flask import Flask, request, jsonify, send_file
-import torch
-import os
-
-from src.tasks import generate_ad, generate_img
-
-assert torch.cuda.is_available()
-torch.cuda.empty_cache()
-
-app = Flask(__name__)
-
-@app.route('/img2img-api', methods=['POST'])
+@app.route('/img2img-api', methods=['GET', 'POST'])
 def img2img_api():
-    try:
-        # Get the inputs from the request
+    if request.method == 'POST':
+         # Get the inputs from the request
         prompt = request.form['prompt']
         image = request.files['image']
         aiimg_hexcode = request.form['aiimg_hexcode']
@@ -105,26 +69,14 @@ def img2img_api():
         button = request.form['button']
 
         gen_image = generate_img(image, prompt, aiimg_hexcode)
-        # ad_image = generate_ad(image, logo, adimg_hexcode, punchline, button)
+        ad_image = generate_ad(gen_image, logo, adimg_hexcode, punchline, button)
 
-        # if gen_image and ad_image:
-        #     # Save the generated ad_image
-        #     save_path = os.path.join("images", "ad_image.png")
-        #     ad_image.save(save_path)
-
-        #     # Return the generated image as a response
-        #     return send_file(save_path, mimetype="image/png")
-        # else:
-        #     return jsonify({"error": "Image generation failed."})
-
-        #  Save the generated ad_images
-        save_path = os.path.join("images", "gen_image.png")
-        gen_image.save(save_path)
-
-        # Return the generated image as a response
-        return send_file(save_path, mimetype="image/png")
-    except Exception as e:
-        return jsonify({"error": str(e)})
+        ad_img_path = os.path.join(cfg['image_upload_folder'], cfg['ad_img_name'])
+        ad_image.save(ad_img_path)
+        
+        return render_template('result.html', result=cfg['ad_img_name'])
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='localhost', port=8080)
